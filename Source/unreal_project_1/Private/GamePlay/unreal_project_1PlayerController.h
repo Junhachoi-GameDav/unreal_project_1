5124ef697e245824311e6a369bbbcbc98194ddb3
janhachoi <wnsgk7654@gmail.com>
2023-07-08 10:48:38 +0900

    7-8 remove source code from thirdperson template, and create cpp classes for unreal_project_1

---
+++ Source/unreal_project_1/Private/GamePlay/unreal_project_1PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "unreal_project_1PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class Aunreal_project_1PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
