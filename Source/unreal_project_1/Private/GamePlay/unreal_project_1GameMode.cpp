048573d6bf26a074af2eb75d7753db5675ddea2d
janhachoi <wnsgk7654@gmail.com>
2023-07-13 01:25:20 +0900

    Revert "7-12 migrate ps_game_mode into cpp"
    This reverts commit 731e5f29a0b4015877b8c7a3613d2a8129b31f04.

--- Source/unreal_project_1/Private/GamePlay/unreal_project_1GameMode.cpp
+++ Source/unreal_project_1/Private/GamePlay/unreal_project_1GameMode.cpp
@@ -1,5 +1,28 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "GamePlay/unreal_project_1GameMode.h"
 
+void Aunreal_project_1GameMode::UpdateRankingCPP()
+{
+	int32 RankingIndex = 0;
+	//Num()은 크기이다. 또는 갯수
+	for (int32 i = 0; i < RankingScoresCPP.Num(); i++)
+	{
+		float element = RankingScoresCPP[i];
+		if (element >= ScoreCPP)
+		{
+			RankingIndex++;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	RankingCPP = RankingIndex + 1;
+	RankingScoresCPP.Insert(ScoreCPP, RankingIndex);
+	RankingTimesCPP.Insert(EndTimeCPP.ToString(), RankingIndex);
+	if (RankingScoresCPP.Num() > 10)
+	{
+		RankingScoresCPP.RemoveAt(RankingScoresCPP.Num() - 1);
+		RankingTimesCPP.RemoveAt(RankingTimesCPP.Num() - 1);
+	}
+}
--- Source/unreal_project_1/Private/GamePlay/unreal_project_1GameMode.h
+++ Source/unreal_project_1/Private/GamePlay/unreal_project_1GameMode.h
@@ -6,12 +6,43 @@
 #include "GameFramework/GameModeBase.h"
 #include "unreal_project_1GameMode.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_SPARSE_DELEGATE(Funreal_project_1GameModeOnEndGame, Aunreal_project_1GameMode, OnEndGameCPP);
 /**
- * 
+ *
  */
 UCLASS()
 class Aunreal_project_1GameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+		void UpdateRankingCPP();
+
+public:
+
+	//델리게이트 = 이벤트 함수(on ~ bind 뭐시기들)
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+		Funreal_project_1GameModeOnEndGame OnEndGameCPP;
+
+protected:
+	UPROPERTY(Category = GameMode, VisibleAnywhere, BlueprintReadWrite)
+		FDateTime StartTimeCPP;
+
+	UPROPERTY(Category = GameMode, VisibleAnywhere, BlueprintReadWrite)
+		FDateTime EndTimeCPP;
+
+	//배열
+	UPROPERTY(Category = GameMode, VisibleAnywhere, BlueprintReadWrite)
+		TArray<float> RankingScoresCPP;
+
+	UPROPERTY(Category = GameMode, VisibleAnywhere, BlueprintReadWrite)
+		TArray<FString> RankingTimesCPP;
+
+	UPROPERTY(Category = GameMode, VisibleAnywhere, BlueprintReadWrite)
+		int32 RankingCPP;
+
+	UPROPERTY(Category = GameMode, VisibleAnywhere, BlueprintReadWrite)
+		float ScoreCPP;
 };
