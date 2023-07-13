8726df5d40341a48f3a6374c043520f278b0ca7d
janhachoi <wnsgk7654@gmail.com>
2023-07-13 12:04:15 +0900

    7-13 migrate unreal_project_1PlayerController into cpp

--- Source/unreal_project_1/Private/GamePlay/unreal_project_1PlayerController.cpp
+++ Source/unreal_project_1/Private/GamePlay/unreal_project_1PlayerController.cpp
@@ -2,4 +2,41 @@
 
 
 #include "GamePlay/unreal_project_1PlayerController.h"
+#include <EnhancedInputSubsystems.h>
+#include "unreal_project_1GameMode.h"
+#include <Kismet/GameplayStatics.h>
 
+void Aunreal_project_1PlayerController::OnEndGameCPP()
+{
+	UUserWidget* Widget = CreateWidget(this, ResultWidget);
+	if (Widget) {
+		//뷰포트에 띄우기
+		Widget->AddToViewport();
+	}
+}
+
+void Aunreal_project_1PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (Subsystem)
+	{
+		//인풋 버튼 매핑
+		Subsystem->AddMappingContext(unreal_project_1InputMappingContext, 0);
+	}
+
+	UUserWidget* Widget = CreateWidget(this, GamePlayWidget);
+	if (Widget) {
+		//뷰포트에 띄우기
+		Widget->AddToViewport();
+	}
+
+	//게임모드 가져오기
+	Aunreal_project_1GameMode* GameMode = Cast<Aunreal_project_1GameMode>(UGameplayStatics::GetGameMode(this));
+	if (GameMode) 
+	{
+		//소스코드에서 델리게이트를 사용하기위한 함수
+		GameMode->OnEndGameCPP.AddUniqueDynamic(this, &Aunreal_project_1PlayerController::OnEndGameCPP);
+	}
+}
--- Source/unreal_project_1/Private/GamePlay/unreal_project_1PlayerController.h
+++ Source/unreal_project_1/Private/GamePlay/unreal_project_1PlayerController.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include <Widget/unreal_project_1GamePlayWidget.h>
+#include <InputMappingContext.h>
 #include "unreal_project_1PlayerController.generated.h"
 
 /**
@@ -14,4 +16,21 @@ class Aunreal_project_1PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	UFUNCTION()
+	void OnEndGameCPP();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UInputMappingContext> unreal_project_1InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UUserWidget> GamePlayWidget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UUserWidget> ResultWidget;
 };
